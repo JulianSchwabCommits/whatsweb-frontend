e80ddfea76c7d7a06ad9b71736a0c727a04205fb
Julian Schwab <jla.schwab@gmail.com>
2025-12-11 14:56:50 +0100

    feat: changed backend url

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://whatsweb-backend.polandcentral.azurecontainer.io:8080');
+const socket = io('https://whatsweb-backend.azurewebsites.net');
 
 
 function App() {
