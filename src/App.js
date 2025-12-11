cb29c759e741217d1e8401b6162bbdf3548eaae6
Julian Schwab <jla.schwab@gmail.com>
2025-12-11 11:48:39 +0100

    feat: changed backend url and added real documentation

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './App.css';
 
-const socket = io('http://whatsweb-backend-1764664439.polandcentral.azurecontainer.io:8080');
+const socket = io('http://whatsweb-backend.polandcentral.azurecontainer.io:8080');
+
 
 function App() {
   const [socketId, setSocketId] = useState('');
