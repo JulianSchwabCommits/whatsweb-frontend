572855609da16c61d9bb8a91da8635c0af9350c7
Julian Schwab <jla.schwab@gmail.com>
2025-12-14 12:09:46 +0100

    feat: error handling for no .env

--- lib/env.ts
+++ lib/env.ts
@@ -1,1 +1,9 @@
-export const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL!;
+const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL;
+
+if (!backendUrl) {
+    throw new Error(
+        'Please create a .env file with NEXT_PUBLIC_BACKEND_URL variable set.'
+    );
+}
+
+export const BACKEND_URL = backendUrl;
